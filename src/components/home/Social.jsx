3797571cbc2fad3cee0daaf97a076eba9a3e40b8
WHB901 <WHB901@gmail.com>
2023-02-28 13:15:58 +0500

    Header is made and HomePage is made, Have to Connect Socials in Social.jsx

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Social = () => { 
+    return (
+        <div className="home__social">
+            <a href="#" className="home__social-icon" target="_blank">
+            <i class="uil uil-facebook"></i>
+            </a>
+            <a href="#" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+            </a>
+            <a href="#" className="home__social-icon" target="_blank">
+            <i class="uil uil-twitter"></i>
+            </a>
+            <a href="#" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+            </a>
+            <a href="#" className="home__social-icon" target="_blank">
+            <i class="uil uil-github"></i>
+            </a>
+        </div>
+
+    );
+
+}
+
+export default Social;
