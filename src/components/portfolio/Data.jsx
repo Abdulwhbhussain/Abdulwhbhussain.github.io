5ee7c577fe57568dc33ea621d5763ba298a12633
WHB901 <WHB901@gmail.com>
2023-03-09 02:04:39 +0500

    portfolio updated and data is commented out

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -6,36 +6,36 @@ import Work5 from "../../assets/work5.jpg";
 
 
 export const projectsData = [
-    {
-      id: 1,
-      image: Work1,
-      title: "Web design",
-      category: "web",
-    },
-    {
-      id: 2,
-      image: Work2,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 3,
-      image: Work3,
-      title: "Brand design",
-      category: "design",
-    },
-    {
-      id: 4,
-      image: Work4,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 5,
-      image: Work5,
-      title: "Web design",
-      category: "web",
-    },
+    // {
+    //   id: 1,
+    //   image: Work1,
+    //   title: "Web design",
+    //   category: "web",
+    // },
+    // {
+    //   id: 2,
+    //   image: Work2,
+    //   title: "App movil",
+    //   category: "app",
+    // },
+    // {
+    //   id: 3,
+    //   image: Work3,
+    //   title: "Brand design",
+    //   category: "design",
+    // },
+    // {
+    //   id: 4,
+    //   image: Work4,
+    //   title: "App movil",
+    //   category: "app",
+    // },
+    // {
+    //   id: 5,
+    //   image: Work5,
+    //   title: "Web design",
+    //   category: "web",
+    // },
   ];
 
 
@@ -44,12 +44,15 @@ export const projectsNav = [
         name: "all",
     },
     {
-        name: "web",
+        name: "frontend",
     },
     {
-        name: "app",
+        name: "backend",
     },
     {
-        name: "design",
+        name: "full stack",
     },
+    {
+      name: "other",
+  },
 ];
