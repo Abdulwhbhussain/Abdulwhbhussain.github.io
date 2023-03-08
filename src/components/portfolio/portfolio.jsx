34be68f624d3b06b5f982370e668a021689c7491
WHB901 <WHB901@gmail.com>
2023-03-09 02:08:13 +0500

    portfolio.jsx updated

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
     return (
         <section className="work section" id="portfolio">
             <h2 className="section__title">Portfolio</h2>
-            <span className="section__subtitle">Most Recent Works</span>
+            <span className="section__subtitle">Recent Work</span>
 
             <Works />
         </section>
